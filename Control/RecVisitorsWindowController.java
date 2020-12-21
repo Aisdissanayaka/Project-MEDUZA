498745830436b1a9f19273f2e32045992a2d1044
Maduka Bennington <w.madhuka@gmail.com>
2020-12-21 17:33:25 +0530

    Receptionist's Dashboard Tables completed and updated/Version01

--- Control/RecVisitorsWindowController.java
+++ Control/RecVisitorsWindowController.java
@@ -7,9 +7,13 @@ package Control;
 
 import static Control.UserLoginController.profilePicture;
 import static Control.UserLoginController.staticUserName;
+import Model.Patient;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +21,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +32,65 @@ import javafx.stage.Stage;
  * @author ASUS
  */
 public class RecVisitorsWindowController extends DashboardUIController implements Initializable {
+    
+      @FXML
+    private TableView<Visitor> visitorTable;
+
+    @FXML
+    private TableColumn<Visitor,String> idCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  nameCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  intimeCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  outTimeCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  docCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  noteCol;
+
+    @FXML
+    private TableColumn<Visitor,String>  optionsCol;
+    
+    
+    @FXML
+    public void viewVisitors(){
+         try {
+          ArrayList<Visitor> visitorArrayList = Visitor.viewVisitor();
+          
+          int postalCount = visitorArrayList.size();
+          
+          
+             System.out.println(postalCount);
+          
+          ObservableList<Visitor> Visitors = FXCollections.observableArrayList();
+          for(int i =0;i< postalCount;i++){
+          Visitors.add(visitorArrayList.get(i));
+      
+          }
+          idCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("nic"));
+          nameCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("name"));
+          intimeCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("inTime"));
+          outTimeCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("outTime"));
+          docCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("document"));
+          noteCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("note"));
+          optionsCol.setCellValueFactory(new PropertyValueFactory<Visitor,String>("options"));
+          
+       
+          visitorTable.setItems(Visitors);
+          
+             
+         }catch(Exception e){}
+    
+    }
+
+   
+ 
     
     //Trigers the add new Visitor Window
     @FXML
