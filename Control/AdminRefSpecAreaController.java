cc99e766da755375553ba05fcacdbb4a906d63e0
prasadchamika98 <74768719+prasadchamika98@users.noreply.github.com>
2020-12-27 15:26:37 +0530

    Codede spec area reference edit admin

--- Control/AdminRefSpecAreaController.java
+++ Control/AdminRefSpecAreaController.java
@@ -5,6 +5,25 @@
  */
 package Control;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 
@@ -15,7 +34,55 @@ import javafx.fxml.Initializable;
  */
 public class AdminRefSpecAreaController extends DashboardUIController implements Initializable {
 
+        //declare variables
+  @FXML
+    private JFXTextField addNewTxt;
+
+    @FXML
+    private JFXTextArea listTxt;
+
+    
+    //add new speciality area to list
+    @FXML
+    void add(ActionEvent event) throws FileNotFoundException, IOException {
+         
+        File file1 = new File("user data\\reference\\category.txt");
+           PrintWriter print1 = new PrintWriter(new FileOutputStream(file1,true));
+           print1.append(addNewTxt.getText()+"\n");
+          
+           print1.close();
+      
+        loadData();
+        addNewTxt.setText(null);
         
-    }  
+    }
+    
+    
+    // view speciality area list
+     private void loadData() throws FileNotFoundException, IOException{
+         
+        Scanner inputFile = new Scanner(new File("user data\\reference\\category.txt"));
+        String list = "";
+        String c;
+        while(inputFile.hasNextLine()){
+            c =(inputFile.nextLine()+ "\n");
+            list += c ; 
+        }
+     listTxt.setText(list);
+     }
+     
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+      try {
+          loadData();
+      } catch (IOException ex) {
+          Logger.getLogger(AdminRefSpecAreaController.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+    }
+
+}  
     
 
