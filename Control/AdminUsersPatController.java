2873c49d6937c7ba4c261b8e0f6eb774af51c8a7
Ashan <ashanisuru1125@gmail.com>
2020-12-28 11:43:59 -0800

    Coded patient edit, password reset methodes in admin tables

--- Control/AdminUsersPatController.java
+++ Control/AdminUsersPatController.java
@@ -5,18 +5,29 @@
  */
 package Control;
 
+import static Control.RecPatientsWindowController.patientID;
+import static Control.UserLoginController.profilePicture;
+import static Control.UserLoginController.staticUserName;
 import Model.MedicalOfficer;
 import Model.Patient;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -56,11 +67,43 @@ public class AdminUsersPatController extends DashboardUIController implements In
     @FXML
     private TableColumn<Patient, String> allergiesCol;
 
+    
     @FXML
-    private TableColumn<Patient, String> pwCol;
-
-    @FXML
-    private TableColumn<Patient, String> optionsCol;
+    void AdminUserPatEdit(ActionEvent event) throws IOException {
+    try{
+        ObservableList<Patient> allUser,singleUser;
+        allUser = patientTable.getItems();
+        singleUser =patientTable.getSelectionModel().getSelectedItems();
+        patientID = patientTable.getSelectionModel().getSelectedItem().getNic();
+        // all items store in static object in patient class 
+        AdminUsersPatEdit.selectUser=(Patient)patientTable.getSelectionModel().getSelectedItem();
+        singleUser.forEach(allUser::remove);
+        // load patient edit window   
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("/View/Dashboards/Admin/AdminUsersPatEdit.fxml"));
+        Parent root = loader.load();
+        DashboardUIController welcome =loader.getController();
+        welcome.showInformation(staticUserName);
+        welcome.showProfilePicture(profilePicture);
+         
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(new Scene(root));
+        window.show();
+        window.centerOnScreen();
+        }catch(Exception e){
+        
+        Alert alert = new Alert(Alert.AlertType.WARNING); //display Warning message
+        alert.setContentText("Selecet Patient and then press edit  button");
+        alert.show();
+       
+       }
+        
+    }
+    
+    
+    
+    
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -87,9 +130,7 @@ public class AdminUsersPatController extends DashboardUIController implements In
           addressCol.setCellValueFactory(new PropertyValueFactory<Patient,String>("address"));
           bloodCol.setCellValueFactory(new PropertyValueFactory<Patient,String>("bloodGroup"));
           allergiesCol.setCellValueFactory(new PropertyValueFactory<Patient,String>("allergies"));
-          pwCol.setCellValueFactory(new PropertyValueFactory<Patient,String>("password"));
-          optionsCol.setCellValueFactory(new PropertyValueFactory<Patient,String>("options"));
-          
+         
        
           patientTable.setItems(patients);
           
--- Control/AdminUsersPatEdit.java
+++ Control/AdminUsersPatEdit.java
@@ -5,7 +5,32 @@
  */
 package Control;
 
+import static Control.RecPatientEditWindowController.selectedUser;
+import static Control.UserLoginController.profilePicture;
+import static Control.UserLoginController.staticUserName;
+import Model.Patient;
+import Model.user;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -13,11 +38,115 @@ import javafx.fxml.Initializable;
  * @author ASUS
  */
 public class AdminUsersPatEdit extends DashboardUIController implements Initializable {
+  @FXML
+    private JFXTextArea address;
 
+    @FXML
+    private JFXTextField lastName;
+
+    @FXML
+    private JFXComboBox<String> gender;
+
+    @FXML
+    private JFXTextField phoneNumber;
+
+    @FXML
+    private JFXComboBox<String> bloodGroup;
+
+    @FXML
+    private JFXTextArea allergies;
+
+    @FXML
+    private JFXTextField nic;
   
+    @FXML
+    private JFXTextField firstName;
+
+    @FXML
+    private JFXTextField dob;  
+    
+    
+   public static Patient selectUser;
+   
+   @FXML
+    private Circle patientProfilePhoto;
+
+    
+      
+   @FXML
+    void updateBtn(ActionEvent event){
+        
+  try {
+            
+    Patient obj = new Patient();
+    obj.editPatient("user data//database//patients.txt", "user data//database//temp.txt", nic.getText(), firstName.getText(), lastName.getText(), gender.getValue(), dob.getText(),
+                    phoneNumber.getText(),address.getText(),bloodGroup.getValue(), allergies.getText());
+    Alert alert = new Alert(Alert.AlertType.INFORMATION); //display Warning message
+    alert.setContentText("Updated patient profile.!");
+    alert.show();
+            
+        } catch (Exception e) {
+        }
         
         
+    }
+   
+    
+   
+    @FXML //Password reset button
+    void passwordResetBtn(ActionEvent event) {
+         try {
+             user obj = new user(); // create object in user class and call password reset method
+             obj.passwordReset("user data\\patient\\credentials\\patientlogin.txt", "user data\\patient\\credentials\\temp.txt", selectUser.getNic());
+             Alert alert = new Alert(Alert.AlertType.INFORMATION); //display Warning message
+             alert.setContentText("You reset"+selectUser.getFName().toUpperCase()+"'s Password..!");
+             alert.show();
+         } catch (Exception e) {
+         }
+
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  @Override
+    public void initialize(URL url, ResourceBundle rb) { 
+    
+      firstName.setText(selectUser.getFName());
+      lastName.setText(selectUser.getLName());
+      gender.setValue(selectUser.getGender());
+      address.setText(selectUser.getAddress());
+      phoneNumber.setText(selectUser.getPhoneNumber());
+      bloodGroup.setValue(selectUser.getBloodGroup());
+      allergies.setText(selectUser.getAllergies());
+      nic.setText(selectUser.getNic());
+      dob.setText(selectUser.getDOB());
         
+      ObservableList<String>list1=FXCollections.observableArrayList("Male","Female");
+       gender.setItems(list1);
        
+       ObservableList<String>list2=FXCollections.observableArrayList("A+","O+","B+","AB+","A-","O-","B-","AB-");
+       bloodGroup.setItems(list2);
+
+      
+              try{
+        FileInputStream input = new FileInputStream("user data\\patient\\photo\\"+selectUser.getNic()+".jpg");
+ 
+        Image img1 = new Image(input);
+     
+        patientProfilePhoto.setFill(new ImagePattern(img1));
+        }catch(Exception e){
+            
+        }
+       
+       
+    }    
     
 }
